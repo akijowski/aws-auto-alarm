d0b34e9e0ff723c48975c3cdeb9f7000ec6c92e7
Adam Kijowski <akijowski@twilio.com>
2024-08-27 15:21:39 -0600

    run lambda locally

--- functions/aws_auto_alarm/main.go
+++ functions/aws_auto_alarm/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"context"
+	"os"
 
-	"github.com/akijowski/aws-auto-alarm/internal/task"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+
+	"github.com/akijowski/aws-auto-alarm/internal/task"
 )
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	ctx := log.Logger.WithContext(context.Background())
+	ctx := zerolog.New(os.Stdout).With().Timestamp().Logger().WithContext(context.Background())
 	handler := &task.AlarmHandler{}
 	lambda.StartWithOptions(handler.Handle, lambda.WithContext(ctx))
 }
--- internal/task/handler.go
+++ internal/task/handler.go
@@ -2,11 +2,9 @@ package task
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 )
 
 const (
@@ -14,12 +12,7 @@ const (
 	eventbridgeEventDetailType = "Tag Change on Resource"
 )
 
-type AlarmCreator interface {
-	Create(ctx context.Context, req *alarmCreationRequest) error
-}
-
 type AlarmHandler struct {
-	alarmCreators map[string]AlarmCreator
 }
 
 type tagChangeDetail struct {
@@ -30,65 +23,11 @@ type tagChangeDetail struct {
 	Tags           map[string]string `json:"tags"`
 }
 
-type alarmCreationRequest struct {
-	tagChange *tagChangeDetail
-	event     *events.EventBridgeEvent
-}
-
 func (h *AlarmHandler) Handle(ctx context.Context, event *events.SQSEvent) (*events.SQSEventResponse, error) {
-	log.Ctx(ctx).Debug().Msg("handling sqs event")
-	batchItemFailures := []events.SQSBatchItemFailure{}
+	log := zerolog.Ctx(ctx).With().
+		Int("sqs_messages_count", len(event.Records)).
+		Logger()
+	log.Info().Msg("Received SQS event")
 
-	for _, record := range event.Records {
-		rCtx := log.Ctx(ctx).With().Str("sqs_message_id", record.MessageId).Logger().WithContext(ctx)
-		request, err := unmarshalRecord(record)
-		if err != nil {
-			failure := events.SQSBatchItemFailure{ItemIdentifier: record.MessageId}
-			batchItemFailures = append(batchItemFailures, failure)
-			log.Ctx(rCtx).Error().Err(err).Msg("unable to unmarshal record")
-			continue
-		}
-
-		if request.event.Source != eventbridgeEventSource || request.event.DetailType != eventbridgeEventDetailType {
-			log.Ctx(rCtx).Warn().Msg("event does not match requirements, dropping message")
-			continue
-		}
-
-		creator, ok := h.alarmCreators[request.tagChange.Service]
-		if !ok {
-			failure := events.SQSBatchItemFailure{ItemIdentifier: record.MessageId}
-			batchItemFailures = append(batchItemFailures, failure)
-			log.Ctx(rCtx).Error().Err(err).Msg("alarm creator not found")
-			continue
-		}
-
-		err = creator.Create(ctx, request)
-		if err != nil {
-			failure := events.SQSBatchItemFailure{ItemIdentifier: record.MessageId}
-			batchItemFailures = append(batchItemFailures, failure)
-			log.Ctx(rCtx).Error().Err(err).Msg("unable to create alarm")
-			continue
-		}
-	}
-
-	return &events.SQSEventResponse{BatchItemFailures: batchItemFailures}, nil
-}
-
-func unmarshalRecord(record events.SQSMessage) (*alarmCreationRequest, error) {
-	var eventbridgeEvent *events.EventBridgeEvent
-	err := json.Unmarshal([]byte(record.Body), &eventbridgeEvent)
-	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal eventbridge event: %w", err)
-	}
-
-	var detail *tagChangeDetail
-	err = json.Unmarshal(eventbridgeEvent.Detail, &detail)
-	if err != nil {
-		return nil, fmt.Errorf("unable to unmarshal eventbridge event detail: %w", err)
-	}
-
-	return &alarmCreationRequest{
-		tagChange: detail,
-		event:     eventbridgeEvent,
-	}, nil
+	return nil, nil
 }
