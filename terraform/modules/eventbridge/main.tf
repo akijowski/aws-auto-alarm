99ddb3d10592368f762e625c0e56e777464bb3cc
Adam Kijowski <akijowski@twilio.com>
2024-08-30 11:52:41 -0600

    update event rule

--- terraform/modules/eventbridge/main.tf
+++ terraform/modules/eventbridge/main.tf
@@ -19,13 +19,24 @@ resource "aws_cloudwatch_event_rule" "this" {
   name        = var.rule_name
   description = "Forward tag-change events to an SQS queue for processing"
   # event_bus_name leave blank for the default bus
+
+  # https://docs.aws.amazon.com/eventbridge/latest/userguide/eb-create-pattern-operators.html
   event_pattern = jsonencode({
     account     = [data.aws_caller_identity.current.account_id]
     source      = ["aws.tag"]
     detail-type = ["Tag Change on Resource"]
     detail = {
       service = var.allowed_services
-      changed-tag-keys = ["AWS_AUTO_ALARM_ENABLED"]
+      # AWS_AUTO_ALARM_ENABLED is present in either changed keys or existing tags
+      "$or" = [
+        {
+          "changed-tag-keys" = ["AWS_AUTO_ALARM_ENABLED"]
+          }, {
+          "tags" = {
+            "AWS_AUTO_ALARM_ENABLED" = [{ "exists" = true }]
+          }
+        }
+      ]
     }
   })
 }
