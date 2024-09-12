d93efe260127c5be56ff3304e7388fc686a42e31
Adam Kijowski <akijowski@twilio.com>
2024-09-12 12:20:04 -0600

    update acceptance test for cli

--- test/acceptance/cli_test.go
+++ test/acceptance/cli_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"testing"
@@ -19,60 +20,63 @@ import (
 	"github.com/akijowski/aws-auto-alarm/internal/config"
 )
 
+type cliTestCase struct {
+	Config *config.Config  `json:"input"`
+	Output json.RawMessage `json:"output"`
+}
+
 func TestOutput(t *testing.T) {
 	t.Parallel()
 
 	cases := []struct {
-		service   string
-		config    func(testing.TB) (*config.Config, error)
-		wantBytes func(testing.TB) ([]byte, error)
+		name     string
+		fileName string
 	}{
 		{
-			service:   "sqs",
-			config:    configFromTestName,
-			wantBytes: outputFromTestName,
+			name:     "sqs",
+			fileName: "fixtures/cli/sqs.json",
 		},
 		{
-			service:   "sqs_delete",
-			config:    configFromTestName,
-			wantBytes: outputFromTestName,
+			name:     "sqs_delete",
+			fileName: "fixtures/cli/sqs_delete.json",
 		},
 		{
-			service:   "sqs_override_dlq",
-			config:    configFromTestName,
-			wantBytes: outputFromTestName,
+			name:     "sqs_override_dlq",
+			fileName: "fixtures/cli/sqs_override_dlq.json",
 		},
 		{
-			service:   "tags",
-			config:    configFromTestName,
-			wantBytes: outputFromTestName,
+			name:     "tags",
+			fileName: "fixtures/cli/tags.json",
 		},
 	}
 
 	for _, tc := range cases {
-		t.Run(tc.service, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
 			tc := tc
 
 			require := require.New(t)
 
-			config, err := tc.config(t)
+			testCase, err := loadTestCase(t, tc.fileName)
 			require.NoError(err)
+
+			config := testCase.Config
+			require.NotNil(config)
+
+			b := testCase.Output
 
 			buf := new(bytes.Buffer)
 
-			ctx := zerolog.New(zerolog.NewConsoleWriter(zerolog.ConsoleTestWriter(t))).
+			ctx := zerolog.New(zerolog.NewTestWriter(t)).
 				With().
 				Caller().
-				Str("arn", config.ParsedARN.String()).
+				Timestamp().
 				Logger().
 				WithContext(context.Background())
 
-			err = cli.New(config, nil, buf).Run(ctx)
-			assert.NoError(t, err)
-
-			b, err := tc.wantBytes(t)
+			c := cli.New(config, nil, buf)
+			err = c.Run(ctx)
 			require.NoError(err)
 
 			if config.Delete {
@@ -138,4 +142,33 @@ func outputFromTestName(t testing.TB) ([]byte, error) {
 	}
 
 	return file, nil
+}
+
+func loadTestCase(t testing.TB, fileName string) (*cliTestCase, error) {
+	t.Helper()
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	tc := new(cliTestCase)
+	if err = json.Unmarshal(b, tc); err != nil {
+		return nil, err
+	}
+
+	if tc.Config.ARN != "" {
+		parsedARN, err := arn.Parse(tc.Config.ARN)
+		if err != nil {
+			return nil, err
+		}
+		tc.Config.ParsedARN = parsedARN
+	}
+
+	return tc, nil
 }
